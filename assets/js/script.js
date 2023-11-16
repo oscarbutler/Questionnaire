092df40d15a2d8d35ab3807e13b3576d7a5c7e9c
Oscar Butler <oscarbutler>
2023-11-16 20:04:57 +0000

    Add style to questions div

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,4 @@ const questionsContainer = document.getElementById('questions');
 const resultsContainer = document.getElementById('results');
 
 const submitContainer = document.getElementById('submit');
+
