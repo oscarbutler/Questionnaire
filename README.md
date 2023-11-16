e0751d98213ff0d1a7e6b6d580e12c5d6bdf4122
Oscar Butler <oscarbutler>
2023-11-16 16:10:54 +0000

    Add variables to javascript

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Questionnaire
+
+## Fonts
---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+const questionsContainer = document.getElementById('questions');
+
+const resultsContainer = document.getElementById('results');
+
+const submitContainer = document.getElementById('submit');
