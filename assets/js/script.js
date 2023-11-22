553983d2a08eba1c80910f3a8a75da6850613d9e
Oscar Butler <oscarbutler>
2023-11-22 19:25:06 +0000

    Add final questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,15 +38,45 @@ const data = [
       'correct': 'Argentina'
     },
     {
-      '': 'When did World War 2 end?',
+      'question': 'When did World War 2 end?',
       'answers': ['1945', '1943', '1951', '1947'],
       'correct': '1945'
     },
     {
-      'question': 'What continent has the highest population',
+      'question': 'What continent has the highest population?',
       'answers': ['North America', 'Africa', 'Europe', 'Asia'],
       'correct': 'Asia'
     },
+    {
+      'question': '',
+      'answers': ['France', 'Germany', 'Argentina', 'England'],
+      'correct': 'Argentina'
+    },
+    {
+      'question': 'Who won the 2022 World Cup?',
+      'answers': ['France', 'Germany', 'Argentina', 'England'],
+      'correct': 'Argentina'
+    },
+    {
+      'question': 'What is the largest mammal in the world?',
+      'answers': ['Elephant', 'Blue Whale', 'Giraffe', 'Hippopotamus'],
+      'correct': 'Blue Whale'
+    },
+    {
+      'question': 'Which planet is known as the Red Planet?',
+      'answers': ['Mars', 'Venus', 'Jupiter', 'Saturn'],
+      'correct': 'Mars'
+    },
+    {
+      'question': 'What is the capital of Australia?',
+      'answers': ['Brisbane', 'Canberra', 'Sydney', 'Melbourne'],
+      'correct': 'Canberra'
+    },
+    {
+      'question': 'What is the largest ocean on Earth?',
+      'answers': ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+      'correct': 'Pacific Ocean'
+    }
   ]
   
   data[0]
