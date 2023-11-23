73ee8cb00318cc587f263fe39e2ea1c963c4f123
Oscar Butler <oscarbutler>
2023-11-23 16:53:09 +0000

    Make div questions bigger

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,61 @@ const resultsContainer = document.getElementById('results');
 
 const submitContainer = document.getElementById('submit');
 
+const data = [
+  {
+    'question': 'What is the worlds smallest country?',
+    'answers': ['Monaco', 'Vatican City', 'Georgia', 'San Marino'],
+    'correct': 'Vatican City'
+  },
+  {
+    'question': 'Who won the 2022 World Cup?',
+    'answers': ['France', 'Germany', 'Argentina', 'England'],
+    'correct': 'Argentina'
+  },
+  {
+    'question': 'When did World War 2 end?',
+    'answers': ['1945', '1943', '1951', '1947'],
+    'correct': '1945'
+  },
+  {
+    'question': 'What continent has the highest population?',
+    'answers': ['North America', 'Africa', 'Europe', 'Asia'],
+    'correct': 'Asia'
+  },
+  {
+    'question': '',
+    'answers': ['France', 'Germany', 'Argentina', 'England'],
+    'correct': 'Argentina'
+  },
+  {
+    'question': 'Who won the 2022 World Cup?',
+    'answers': ['France', 'Germany', 'Argentina', 'England'],
+    'correct': 'Argentina'
+  },
+  {
+    'question': 'What is the largest mammal in the world?',
+    'answers': ['Elephant', 'Blue Whale', 'Giraffe', 'Hippopotamus'],
+    'correct': 'Blue Whale'
+  },
+  {
+    'question': 'Which planet is known as the Red Planet?',
+    'answers': ['Mars', 'Venus', 'Jupiter', 'Saturn'],
+    'correct': 'Mars'
+  },
+  {
+    'question': 'What is the capital of Australia?',
+    'answers': ['Brisbane', 'Canberra', 'Sydney', 'Melbourne'],
+    'correct': 'Canberra'
+  },
+  {
+    'question': 'What is the largest ocean on Earth?',
+    'answers': ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+    'correct': 'Pacific Ocean'
+  }
+]
+
+data[0]
+
 function buildQuestions(){}
 
 function showResults(){}
@@ -26,57 +81,3 @@ function checkAnswer(){
 
 }
 
-const data = [
-    {
-      'question': 'What is the worlds smallest country?',
-      'answers': ['Monaco', 'Vatican City', 'Georgia', 'San Marino'],
-      'correct': 'Vatican City'
-    },
-    {
-      'question': 'Who won the 2022 World Cup?',
-      'answers': ['France', 'Germany', 'Argentina', 'England'],
-      'correct': 'Argentina'
-    },
-    {
-      'question': 'When did World War 2 end?',
-      'answers': ['1945', '1943', '1951', '1947'],
-      'correct': '1945'
-    },
-    {
-      'question': 'What continent has the highest population?',
-      'answers': ['North America', 'Africa', 'Europe', 'Asia'],
-      'correct': 'Asia'
-    },
-    {
-      'question': '',
-      'answers': ['France', 'Germany', 'Argentina', 'England'],
-      'correct': 'Argentina'
-    },
-    {
-      'question': 'Who won the 2022 World Cup?',
-      'answers': ['France', 'Germany', 'Argentina', 'England'],
-      'correct': 'Argentina'
-    },
-    {
-      'question': 'What is the largest mammal in the world?',
-      'answers': ['Elephant', 'Blue Whale', 'Giraffe', 'Hippopotamus'],
-      'correct': 'Blue Whale'
-    },
-    {
-      'question': 'Which planet is known as the Red Planet?',
-      'answers': ['Mars', 'Venus', 'Jupiter', 'Saturn'],
-      'correct': 'Mars'
-    },
-    {
-      'question': 'What is the capital of Australia?',
-      'answers': ['Brisbane', 'Canberra', 'Sydney', 'Melbourne'],
-      'correct': 'Canberra'
-    },
-    {
-      'question': 'What is the largest ocean on Earth?',
-      'answers': ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
-      'correct': 'Pacific Ocean'
-    }
-  ]
-  
-  data[0]
