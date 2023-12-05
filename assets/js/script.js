a37150e38d38086f84be2bf4b78536c1464b1764
Oscar Butler <oscarbutler>
2023-12-05 15:38:23 +0000

    Add more questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ let currentQuestion = [currentQuestionIndex];
 
 const data = [
   {
-    'question': 'What is the worlds smallest country?',
+    'question': 'What is the world\'s smallest country?',
     'answers': ['Monaco', 'Vatican City', 'Georgia', 'San Marino'],
     'correct': 'Vatican City'
   },
@@ -70,8 +70,59 @@ const data = [
     'question': 'What is the largest ocean on Earth?',
     'answers': ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
     'correct': 'Pacific Ocean'
+  },
+  {
+    'question': 'Which element has the chemical symbol "O"?',
+    'answers': ['Oxygen', 'Gold', 'Iron', 'Silver'],
+    'correct': 'Oxygen'
+  },
+  {
+    'question': 'Who wrote the play "Romeo and Juliet"?',
+    'answers': ['Charles Dickens', 'Jane Austen', 'William Shakespeare', 'Homer'],
+    'correct': 'William Shakespeare'
+  },
+  {
+    'question': 'In what year did the Titanic sink?',
+    'answers': ['1912', '1905', '1920', '1898'],
+    'correct': '1912'
+  },
+  {
+    'question': 'Which country is known as the Land of the Rising Sun?',
+    'answers': ['China', 'Japan', 'South Korea', 'Vietnam'],
+    'correct': 'Japan'
+  },
+  {
+    'question': 'What is the currency of Switzerland?',
+    'answers': ['Euro', 'Pound Sterling', 'Swiss Franc', 'Dollar'],
+    'correct': 'Swiss Franc'
+  },
+  {
+    'question': 'Who painted the Mona Lisa?',
+    'answers': ['Vincent van Gogh', 'Leonardo da Vinci', 'Pablo Picasso', 'Claude Monet'],
+    'correct': 'Leonardo da Vinci'
+  },
+  {
+    'question': 'Which animal is known as the "King of the Jungle"?',
+    'answers': ['Lion', 'Tiger', 'Leopard', 'Cheetah'],
+    'correct': 'Lion'
+  },
+  {
+    'question': 'What is the largest desert in the world?',
+    'answers': ['Gobi Desert', 'Sahara Desert', 'Antarctica', 'Arabian Desert'],
+    'correct': 'Antarctica'
+  },
+  {
+    'question': 'Who was the first woman to win a Nobel Prize?',
+    'answers': ['Marie Curie', 'Rosalind Franklin', 'Ada Lovelace', 'Jane Goodall'],
+    'correct': 'Marie Curie'
+  },
+  {
+    'question': 'What is the capital of Canada?',
+    'answers': ['Toronto', 'Montreal', 'Ottawa', 'Vancouver'],
+    'correct': 'Ottawa'
   }
-]
+];
+
 
 const nextButton = document.createElement('button');
 nextButton.textContent = 'Next Question';
